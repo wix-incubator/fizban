6dc9b5a01531169f2845805ec1c3449e98a54035
Yehonatan Daniv <yehonatand@wix.com>
2023-03-19 12:23:34 +0200

    Added types

---
+++ types.d.ts
@@ -0,0 +1,14 @@
+declare type scrollConfig = {
+  scenes: ScrollScene;
+  transitionActive?: boolean;
+  transitionFriction?: number;
+}
+
+declare type ScrollScene = {
+  effect: (scene: ScrollScene, progress: number) => void;
+  start?: number;
+  duration?: number;
+  end?: number;
+  disabled?: boolean;
+  viewSource?: Element;
+}
