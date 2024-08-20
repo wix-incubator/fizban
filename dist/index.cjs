9e2cc2b225c7cd972efde490e85343482c9d3f7f
ameerabuf <ameerf@wix.com>
2024-08-20 15:13:22 +0300

    build files

--- dist/index.cjs
+++ dist/index.cjs
@@ -556,6 +556,7 @@ function getController (config) {
    * Prepare scenes data.
    */
   _config.scenes = Object.values(
+    // TODO(ameerf): find a polyfill and use groupBy instead of following reduce
     config.scenes.reduce((acc, scene, index) => {
       const key = scene.groupId ? `group-${scene.groupId}` : String(index);
       if (acc[key]) {
